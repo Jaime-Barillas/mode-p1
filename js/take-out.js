3ec5545b88664a54f439ea5a6a22e8c93dd3bb0d
Jaime-Barillas <36712691+Jaime-Barillas@users.noreply.github.com>
2019-11-20 02:23:53 -0500

    Add button click event handlers and generate multiple order items

--- js/take-out.js
+++ js/take-out.js
@@ -30,9 +30,53 @@ function createOrderCard(order) {
     return card;
 }
 
+function randOrderNumber() {
+    function randChar() {
+        let chars = 'ABCEGHJKLMNPRSTVWXYZ0123456789';
+        let index = Math.floor(Math.random() * Math.floor(chars.length));
+        return chars[index];
+    }
+
+    let orderNumber = '';
+    for (var i = 0; i < 6; i++) {
+        orderNumber += randChar();
+    }
+    return orderNumber;
+}
+
 function main() {
     let orderList = document.getElementById('lstOrder');
-    orderList.append(createOrderCard(getOrders()[0]));
+    let orderNotification = document.getElementById('ntfOrder');
+
+    // Button handlers.
+    let btnClear = document.getElementById('btnClear');
+    btnClear.addEventListener('click', function() {
+        // Remove any outstanding order number notifications.
+        while (orderNotification.firstChild) {
+            orderNotification.firstChild.remove();
+        }
+
+        // Remove any outstanding menu items from the list.
+        while(orderList.firstChild) {
+            orderList.firstChild.remove();
+        }
+    });
+
+    let btnOrder = document.getElementById('btnOrder');
+    btnOrder.addEventListener('click', function() {
+        let label = document.createElement('h2');
+        let orderNumber = document.createTextNode(randOrderNumber());
+
+        label.textContent = 'Your Order Code Is:';
+        label.classList.add('is-title');
+        orderNotification.appendChild(label);
+        orderNotification.appendChild(orderNumber);
+    });
+
+    // Generate list of orders.
+    for (order of getOrders()) {
+        orderList.append(createOrderCard(order));
+    }
 }
 
 window.addEventListener('load', main);
