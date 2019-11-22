14eb8f6d86de54257537659309954b70eee9ac93
Kenrick <>
2019-11-21 21:03:35 -0500

    Add contact-us and locations pages

---
+++ js/info.js
@@ -0,0 +1,37 @@
+var locations = [
+    {
+        City: "Goodwood",
+        Address: "125 Front Street",
+        Postal_Code: "L0C 1A0"
+    },
+    {
+        City: "Erin",
+        Address: "460 Cross Street",
+        Postal_Code: "N0B 0A9"
+    },
+    {
+        City: "Ekfrid",
+        Address: "2 Switzer Drive",
+        Postal_Code: "N0L 1A0"
+    },
+    {
+        City: "Dongola",
+        Address: "7142 Baseline Road",
+        Postal_Code: "K2C 0C8"
+    },
+    {
+        City: "Cloyne",
+        Address: "7 Wickware Lane",
+        Postal_Code: "K0C 1X0"
+    },
+    {
+        City: "Glen Tay",
+        Address: "938 Harper Road",
+        Postal_Code: "P5N 2J5"
+    },
+    {
+        City: "Tincap",
+        Address: "64 Kilkenny Road",
+        Postal_Code: "K0E 1M0"
+    }
+];
